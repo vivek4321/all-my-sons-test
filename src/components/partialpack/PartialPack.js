a93daa652e6a30db66d9bf1d10426e3443b9d80d
vivek <vivekvivek9866@gmail.com>
2019-08-28 01:15:14 -0400

    added side price texts

--- src/components/partialpack/PartialPack.js
+++ src/components/partialpack/PartialPack.js
@@ -3,6 +3,7 @@ import logoBrand from '../../assets/img/logo-brand.png';
 import {withRouter, Link} from 'react-router-dom';
 import { IoIosArrowForward, IoIosCheckmarkCircleOutline } from "react-icons/io";
 import { ReactComponent as Chevron} from '../../assets/icons/chevron-right.svg';
+import PriceCal from './../priceCalu/PriceCal'
 import './PartialPack.css';
 
 
@@ -13,6 +14,7 @@ export class PartialPacking extends Component {
         <div className="list-item-pack">
           <h2>PARTIAL PACK</h2>
           <Chevron className="arrow-icon-pack"></Chevron>
+          <PriceCal bestPrice="Y" price="1223"></PriceCal>
           <div>4 Movers</div>
           <div>3 Hours</div>
           <hr></hr>
@@ -23,6 +25,7 @@ export class PartialPacking extends Component {
         <div className="list-item-pack">
           <h2>FULL PACK</h2>
           <Chevron className="arrow-icon-pack"></Chevron>
+          <PriceCal bestPrice="N" price="2123"></PriceCal>
           <div>6 Movers</div>
           <div>4 Hours</div>
           <hr></hr>
@@ -33,12 +36,13 @@ export class PartialPacking extends Component {
         <div className="list-item-pack">
           <h2>I'LL PACK MYSELF</h2>
           <Chevron className="arrow-icon-pack"></Chevron>
+          <PriceCal bestPrice="N" price="1234"></PriceCal>
           <div>2 Movers</div>
           <div>2 Hours</div>
         </div>
         <div className="list-item-pack">
           <h2>CUSTOMIZE</h2>
-          <Chevron className="arrow-icon-pack"></Chevron>
+          <Chevron className="arrow-icon-pack customize"></Chevron>
         </div>
      
         </div>
---
+++ src/components/priceCalu/PriceCal.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import './PriceCal.css'
+
+export class PriceCal extends Component {
+  render() {
+    return (
+      <div className="price-cal">
+        <div className={(this.props.bestPrice == 'Y') ? 'best-value' : 'display-none'}>Best Value</div>
+         <div className="dollor-amount">$ {this.props.price}</div>
+         <div className="add-hour">*$150 for each additional hour </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(PriceCal);
