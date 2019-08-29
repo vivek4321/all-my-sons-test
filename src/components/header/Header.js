deb899c98ffc9a5bf226919b9aad64811f4f4a47
vivek <vivekvivek9866@gmail.com>
2019-08-29 09:54:55 -0400

    added media quiries for mobile

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import logoBrand from '../../assets/img/logo-brand.png';
 import './Header.css';
-// import { FaAngleLeft } from 'react-icons/fa';
-// import { FaPhone } from 'react-icons/fa';
+import { FaAngleLeft } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
 import { withRouter, Link } from 'react-router-dom';
 // import { IoIosArrowBack} from "react-icons/io";
 const images = require.context('../../assets/', true);
 
 export class Header extends Component {
   render() {
-    let callus = images(`./img/callus.png`);
-    let leftar = images(`./img/leftarr.png`);
+    let callus = images(`./icons/phone-call.png`);
+    let leftar = images(`./icons/left-arrow.png`);
     return (
       <div className="nav-bar">
         <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
           <img src={leftar} className="fa-angle-left" alt="logo" />
         </Link>
+        {/* <Link style={{ textDecoration: 'none', color: 'black' }} to="/"><FaAngleLeft size={70} className="fa-angle-left" ></FaAngleLeft></Link> */}
         <img src={logoBrand} className="all-sons-logo" alt="logo" />
         <img src={callus} className="fa-phone-right" alt="logo" />
+        <span className="callus-text">CALL US</span>
+        {/* <FaPhone size={40} className="fa-phone-right" ></FaPhone> */}
       </div>
     );
   }
--- src/components/partialpack/PartialPack.js
+++ src/components/partialpack/PartialPack.js
@@ -26,36 +26,36 @@ export class PartialPacking extends Component {
     return (
       <div>
         <div className="list-item-pack side-green-border">
-          <h2>PARTIAL PACK</h2>
+          <h2 class="text-size-mobile">PARTIAL PACK</h2>
           <Chevron className="arrow-icon-pack"></Chevron>
           <PriceCal bestPrice="Y" price={this.checkUndefined(this.props.PriceReducer.result.partialPrice)}></PriceCal>
           <div>4 Movers</div>
           <div>3 Hours</div>
           <hr></hr>
-          <div><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>Included all boxes & tapes</span></div>
-          <div><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>We will doall packing</span></div>
-          <div><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>quit pad wrap to all your furiture</span></div>
+          <div className="font-14"><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>  Included all boxes & tapes</span></div>
+          <div className="font-14"><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>  We will doall packing</span></div>
+          <div className="font-14"><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>  quit pad wrap to all your furiture</span></div>
         </div>
         <div className="list-item-pack">
-          <h2>FULL PACK</h2>
+          <h2 class="text-size-mobile">FULL PACK</h2>
           <Chevron className="arrow-icon-pack"></Chevron>
           <PriceCal bestPrice="N" price={this.props.PriceReducer.result.fullPrice}></PriceCal>
           <div>6 Movers</div>
           <div>4 Hours</div>
           <hr></hr>
-          <div><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>Included all boxes & tapes</span></div>
-          <div><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>We will doall packing</span></div>
-          <div><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>quit pad wrap to all your furiture</span></div>
+          <div className="font-14"><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>  Included all boxes & tapes</span></div>
+          <div className="font-14"><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>  We will doall packing</span></div>
+          <div className="font-14"><IoIosCheckmarkCircleOutline></IoIosCheckmarkCircleOutline><span>  quit pad wrap to all your furiture</span></div>
         </div>
         <div className="list-item-pack">
-          <h2>I'LL PACK MYSELF</h2>
+          <h2 class="text-size-mobile">I'LL PACK MYSELF</h2>
           <Chevron className="arrow-icon-pack"></Chevron>
           <PriceCal bestPrice="N" price={this.props.PriceReducer.result.myPrice}></PriceCal>
           <div>2 Movers</div>
           <div>2 Hours</div>
         </div>
         <div className="list-item-pack">
-          <h2>CUSTOMIZE</h2>
+          <h2 class="text-size-mobile">CUSTOMIZE</h2>
           <Chevron className="arrow-icon-pack customize"></Chevron>
         </div>
       </div>
